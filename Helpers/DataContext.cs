2b70647993cc6c29837f9811ed302e6f2b90bd90
treetrunkz <ecarl@mail.greenriver.edu>
2023-08-26 09:11:43 -0700

    User Entities, DB Entities Completed - Database Models In Progress

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,23 @@
+namespace WebApi.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sql server database
+        options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Sound> Sounds { get; set; }
+}
