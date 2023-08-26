64e14966ee21d308804dd5ed5c01a286cb71d454
treetrunkz <ecarl@mail.greenriver.edu>
2023-08-26 09:11:43 -0700

    User Entities, DB Entities Completed - Database Models In Progress

---
+++ Entities/Sound.cs
@@ -0,0 +1,20 @@
+namespace WebApi.Entities;
+
+using System.Text.Json.Serialization;
+
+public class Sound
+{
+    public string Name { get; set; }
+    public float Coordinate_X { get; set; }
+    public float Coordinate_Y { get; set; }
+    public int Width_m { get; set; }
+    public int Depth_m { get; set; }
+    public string Wildlife { get; set; }
+    public string Biome { get; set; }
+    public string Health { get; set; }
+    public float Speed { get; set; }
+    public string InvasiveSpecies { get; set; }
+    public bool IsResidential { get; set; }
+    public bool IsPublicLands { get; set; }
+    public string Topography { get; set; }
+}
---
+++ Models/Sounds/UpdateRequest.cs
@@ -0,0 +1,18 @@
+namespace WebApi.Models.Sounds;
+
+public class UpdateRequest
+{
+    public string Name { get; set; }
+    public float Coordinate_X { get; set; }
+    public float Coordinate_Y { get; set; }
+    public int Width_m { get; set; }
+    public int Depth_m { get; set; }
+    public string Wildlife { get; set; }
+    public string Biome { get; set; }
+    public string Health { get; set; }
+    public float Speed { get; set; }
+    public string InvasiveSpecies { get; set; }
+    public bool IsResidential { get; set; }
+    public bool IsPublicLands { get; set; }
+    public string Topography { get; set; }
+}
